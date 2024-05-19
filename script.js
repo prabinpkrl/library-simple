a6500181f669fea830201a1d805e8f9818e3275c
Prabin Pokhrel <prabinpokhrel7722@gmail.com>
2024-05-19 12:39:41 +0545

    updated

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
-const myLibrary= [];
+const myLibrary = [];
 
 function Book(title,author){
     this.title= title;
     this.author= author;
-    // this.isread= isread;
-    // this.genere= genere;
+    // this.id = id;
 }
 
-const libraryForm = document.querySelector('form');
+const form = document.querySelector('form');
+form.addEventListener("submit", Event => {
+ Event.preventDefault();
 
-libraryForm.addEventListener('submit', e => {
-    e.preventDefult();
+ const bookTitle = document.querySelector('#booktitle').value;
+ const bookAuthor = document.querySelector('#bookauthor').value;
 
-    const name= document.querySelector('#booktitle').value;
-    const author= document.querySelector('#bookauthor').value;
-    // const genere= document.querySelector('#bookgenere');
-    // let name = bookname.value;
-    // let author = bookauthor.value;
+ const newbook = new Book(bookTitle,bookAuthor);
 
-    const newbook = new Book(name,author);
-    console.log(newbook);
+ addBookToLibrary(newbook);
 
-    addBookToLibrary(newbook);
-
-
+ form.reset;
+ 
 });
 
- function addBookToLibrary(newbook){
 
+
+function addBookToLibrary(newbook){
     myLibrary.push(newbook);
-   
-    dispalyBooks();
-    clearfield();
- 
- }
-function dispalyBooks(){
-    const tablebody = document.getElementById('table-body');
-    tablebody.querySelectorAll('tr').forEach(el => el.remove());
-
-    myLibrary.forEach(newbook => {
-        createRow(newbook);
-    });
-
+    
+    displayBooks();
+        
 }
-function createRow(newbook){
-    // let addToHtml;
-    const tableBody = document.getElementById('table-body');
+
+
+function displayBooks(){
+    const tbody = document.querySelector('#table-body');
+    tbody.innerHTML= '';
+
+    myLibrary.forEach(book => {
+        createrow(book)
+    });
+}
+
+function createrow(book){
+    const tbody = document.querySelector('#table-body');
     const row = document.createElement('tr');
-    row.innerHTML =`
-                    <td>${newbook.title}</td>
-                    <td>${newbook.author}</td>
-                    <td id='delete'>Delete</td>                
-            `;
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>  
+    `;
+    tbody.appendChild(row);
+
 }
